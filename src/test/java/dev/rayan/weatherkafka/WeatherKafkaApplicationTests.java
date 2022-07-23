ac9eb6c7ba10b14866c6ca62426f3a82d1eb6b0f
RayanAlkhelaiwi <r.n.m.k@live.com>
2022-07-24 01:10:06 +0300

    Inital Spring-boot kafka weather app commit

---
+++ src/test/java/dev/rayan/weatherkafka/WeatherKafkaApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.rayan.weatherkafka;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WeatherKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

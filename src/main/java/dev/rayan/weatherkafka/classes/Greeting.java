7e5241d4e2b5a187556b1e4a2fd9ed76b1d59f9d
RayanAlkhelaiwi <r.n.m.k@live.com>
2022-07-24 01:12:34 +0300

    Add sample greeting class

---
+++ src/main/java/dev/rayan/weatherkafka/classes/Greeting.java
@@ -0,0 +1,14 @@
+package dev.rayan.weatherkafka.classes;
+
+public class Greeting {
+
+	private final String content;
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}

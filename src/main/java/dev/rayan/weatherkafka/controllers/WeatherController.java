8975c6d8c83a358f23dcac57b43eec5ad195f612
RayanAlkhelaiwi <r.n.m.k@live.com>
2022-07-28 14:46:22 +0300

    Add querying endpoints

--- src/main/java/dev/rayan/weatherkafka/controllers/WeatherController.java
+++ src/main/java/dev/rayan/weatherkafka/controllers/WeatherController.java
@@ -1,9 +1,13 @@
 package dev.rayan.weatherkafka.controllers;
 
 import dev.rayan.weatherkafka.classes.Weather;
+import dev.rayan.weatherkafka.repository.WeatherCRUD;
 import dev.rayan.weatherkafka.services.WeatherProducerService;
 import dev.rayan.weatherkafka.services.WeatherConsumerService;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManager.*;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +22,8 @@ public class WeatherController {
   @Autowired
   private WeatherProducerService weatherProducerService;
   private WeatherConsumerService weatherConsumerService;
+  // private WeatherCRUD weatherCRUD;
+  private EntityManager em;
 
   // POST /weather/create
   @PostMapping(value = "/create")
@@ -38,12 +44,33 @@ public class WeatherController {
    *  );
    */
 
+  // GET /weather/latestWeather
+  @GetMapping("/latest")
+  public List<Weather> latestWeather() {
+
+    Query queryResult = (Query) em.createNativeQuery(
+      "SELECT location, degree FROM locations_weather WHERE id IN (SELECT MAX(id) FROM locations_weather GROUP BY location)");
+    List<Weather> latestWeather = queryResult.getResultList();
+    return latestWeather;
+  }
+
   /*
    * Endpoint to get average weather of specific location
    *  SELECT location, AVG(degree)
    *  FROM locations_weather
+   *  WHERE location = :#{location}
    *  GROUP BY location;
    */
+
+  // GET /weather/avg
+  @GetMapping("/avg")
+  public Weather avgWeather(@RequestParam(value = "location", defaultValue = "AlrawabiIoT") String location) {
+
+    Query queryResult = em.createNativeQuery(
+      "SELECT location, AVG(degree) FROM locations_weather GROUP BY location");
+    Weather latestWeather = queryResult.getResult();
+    return latestWeather;
+  }
 
   // GET /weather/all
   @GetMapping("/all")
--- src/main/java/dev/rayan/weatherkafka/services/WeatherConsumerService.java
+++ src/main/java/dev/rayan/weatherkafka/services/WeatherConsumerService.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
